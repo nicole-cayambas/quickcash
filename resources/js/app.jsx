efb8c2aed8fefa93b0b4611a69bb5e447873f6c9
nicole <nicolescayambas@gmail.com>
2022-08-11 11:17:32 +0800

    #1 setup development environment adding laravel framework files initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PrimaryLayout from './layouts/PrimaryLayout';
+import '../css/app.css'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { BrowserRouter } from "react-router-dom";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <BrowserRouter>
+            <PrimaryLayout />
+        </BrowserRouter>
+    </ThemeProvider>
+</React.StrictMode>
+)
