c6c328c982603b69db75832c7bb2def726e5aa80
nicole <nicolescayambas@gmail.com>
2022-08-16 10:45:16 +0800

    #9 setup roles and permissions closes #9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use Database\Factories\AccountFactory;
 use Database\Factories\CompanyFactory;
 use Database\Factories\LoanFactory;
-use Database\Factories\UserFactory;
+use App\Models\User;
 
 
 class DatabaseSeeder extends Seeder
@@ -20,10 +20,39 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        UserFactory::new()->count(1)->create();
+        $user1 = User::factory()->create([
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+        $user2 = User::factory()->create([
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'email' => 'test2@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+        $user3 = User::factory()->create([
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'email' => 'test3@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+        $user4 = User::factory()->create([
+            'first_name' => 'Test',
+            'last_name' => 'User',
+            'email' => 'test4@example.com',
+            'password' => bcrypt('secret'),
+        ]);
 
         AccountFactory::new()->count(10)->create();
         CompanyFactory::new()->count(10)->create();
         LoanFactory::new()->count(10)->create();
+        
+        $this->call(RoleAndPermissionSeeder::class);
+        $user1->assignRole('Owner');
+        $user2->assignRole('Administrator');
+        $user3->assignRole('Payroll_Officer');
+        $user4->assignRole('Employee');
     }
 }
---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        /* CREATE PERMISSIONS */
+
+        // ADMINISTRATORS
+        Permission::create(['name' => 'view-administrator']);
+        Permission::create(['name' => 'create-administrator']);
+        Permission::create(['name' => 'edit-administrator']);
+        
+        // EMPLOYEES
+        Permission::create(['name' => 'view-employee']);
+        Permission::create(['name' => 'create-employee']);
+        Permission::create(['name' => 'edit-employee']);
+
+        // PAYROLL OFFICERS
+        Permission::create(['name' => 'view-payroll']);
+        Permission::create(['name' => 'create-payroll']);
+        Permission::create(['name' => 'edit-payroll']);
+
+        // COMPANIES
+        Permission::create(['name' => 'create-company']);
+        Permission::create(['name' => 'edit-company']);
+        
+        // LOANS
+        Permission::create(['name' => 'view-employees-loan']);
+        Permission::create(['name' => 'view-companys-loan']);
+        Permission::create(['name' => 'create-loan']);
+        Permission::create(['name' => 'edit-loan']);
+        Permission::create(['name' => 'acknowledge-loan']);
+        Permission::create(['name' => 'request-loan']);
+        Permission::create(['name' => 'view-own-loan']);
+
+        // ACCOUNTS
+        Permission::create(['name' => 'edit-account']); // for modifying capital
+
+        // RECEIVABLES
+        Permission::create(['name' => 'view-receivables']);
+
+
+
+        /* CREATE ROLES */
+        $ownerRole = Role::create(['name' => 'Owner']);
+        $adminRole = Role::create(['name' => 'Administrator']);
+        $payrollRole = Role::create(['name' => 'Payroll_Officer']);
+        $employeeRole = Role::create(['name' => 'Employee']);
+
+
+        /* ASSIGN PERMISSIONS TO ROLES */
+
+
+        $ownerRole->givePermissionTo([
+            'view-administrator', 
+            'create-administrator', 
+            'edit-administrator',
+            'create-company',
+            'edit-company',
+            'view-employee',
+            'create-employee',
+            'edit-employee',
+            'view-employees-loan',
+            'view-companys-loan',
+            'create-loan',
+            'acknowledge-loan',
+            'edit-account',
+            'view-receivables'
+        ]);
+
+        $adminRole->givePermissionTo([
+            'create-company',
+            'edit-company',
+            'view-payroll',
+            'create-payroll',
+            'edit-payroll',
+            'view-employee',
+            'create-employee',
+            'edit-employee',
+            'view-employees-loan',
+            'view-companys-loan',
+            'acknowledge-loan',
+            'edit-account',
+            'view-receivables'
+        ]);
+
+        $payrollRole->givePermissionTo([
+            'create-company',
+            'edit-company',
+            'view-employee',
+            'create-employee',
+            'edit-employee',
+            'view-companys-loan',
+            'create-loan',
+            'edit-loan',
+            'view-receivables'
+        ]);
+
+        $employeeRole->givePermissionTo([
+            'request-loan',
+            'view-own-loan'
+        ]);
+
+
+        /* ASSIGN ROLES TO USERS */
+
+        $user = \App\Models\User::find(1);
+        $user->assignRole($ownerRole);
+    }
+}
